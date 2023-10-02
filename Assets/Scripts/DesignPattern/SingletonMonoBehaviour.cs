8dc249839a4add0d77abdc1374022fb69d7e3521
MODOKI\baruo <obaru.namu1101@gmail.com>
2023-10-02 18:19:51 +0900

    [add]Singletonパターンのスクリプト作成

---
+++ Assets/Scripts/DesignPattern/SingletonMonoBehaviour.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    Debug.LogError(typeof(T) + "Ç™ÉVÅ[ÉìÇ…ë∂ç›ÇµÇ‹ÇπÇÒ");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
