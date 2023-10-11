5c7cedef39bd30b6bd29ebd1e61ab095790cb7f7
MODOKI\baruo <obaru.namu1101@gmail.com>
2023-10-11 16:55:27 +0900

    [update]ハイスコア表示、音関連の処理追加

--- Assets/Scripts/Title/Title.cs
+++ Assets/Scripts/Title/Title.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// タイトル画面での処理
 /// </summary>
 public class Title : MonoBehaviour
 {
+    #region serialize
+    [Tooltip("ハイスコアテキスト")]
+    [SerializeField]
+    private TextMeshProUGUI _highScoreText = default;
+    #endregion
+
     #region unity methods
+    private void Start()
+    {
+        AudioManager.Instance.PlayBGM(AudioManager.Instance.TitleBGM);
+        _highScoreText.text = "HighScore:" + GameManager.Instance.HighScore.ToString();
+    }
+
     private void Update()
     {
         //何か入力されたら
         if (Input.anyKeyDown)
         {
-            //InGameシーンへ
-            SceneController.Instance.ChangeScene(SceneName.TitleScene, SceneName.InGameScene);
+            StartCoroutine(ChangeSceneCoroutine());
         }
+    }
+    #endregion
+
+    #region coroutine method
+    /// <summary>
+    /// シーン切り替え用のコルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ChangeSceneCoroutine()
+    {
+        //SE再生
+        AudioManager.Instance.PlaySE(AudioManager.Instance.EnterSE);
+
+        //SE再生終了を待つ
+        float time = AudioManager.Instance.EnterSE.length;
+        yield return new WaitForSeconds(time);
+
+        //BGMストップ
+        AudioManager.Instance.StopBGM();
+
+        //InGameシーンへ
+        SceneController.Instance.ChangeScene(SceneName.TitleScene, SceneName.InGameScene);
     }
     #endregion
 }
